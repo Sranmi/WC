8b546da4b9d3a23466e76e8740d023e84d4fd13c
3216005167 <460971714@qq.com>
2018-09-14 18:17:06 +0800

    软件工程WC markdown 完成

--- WC window framework/WC window framework/Form1.cs
+++ WC window framework/WC window framework/Form1.cs
@@ -14,11 +14,18 @@ namespace WC_window_framework
     public partial class Form1 : Form
     {
         private int charcount;
-        private int workcount;
+        private int wordcount;
         private int linecount;
         private string filename;
         private string[] parameter;
-        private string showdata=null ;
+        private string showdata = null;
+
+        private int codelinecount;
+        private int notelinecount;
+        private int nulllinecount;
+
+        //private bool iss;
+        private string format;
         public Form1()
         {
             InitializeComponent();
@@ -27,8 +34,12 @@ namespace WC_window_framework
         private void Form1_Load(object sender, EventArgs e)
         {
             this.charcount = 0;
-            this.workcount = 0;
+            this.wordcount = 0;
             this.linecount = 0;
+            this.codelinecount = 0;
+            this.nulllinecount = 0;
+            this.notelinecount = 0;
+            // this.iss = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -37,7 +48,7 @@ namespace WC_window_framework
             if (showdata != null)
                 MessageBox.Show(showdata);
             ClearData();
-            
+
         }
         /// <summary>
         /// 查询总操作流程
@@ -50,6 +61,65 @@ namespace WC_window_framework
 
             foreach (var s in parameter)
             {
+                if (s != "-c" && s != "-w" && s != "-l" && s != "-s" && s != "-a" && s != "-x")
+                {
+                    MessageBox.Show("参数输入有误，请重新输入，注意多参数之间必须用空格隔开");
+                    break;
+                }
+                //参数为-s时遍历其文件夹下所有符合条件的文件
+                //if (s == "-s")
+                //{
+                //    //读取条件文件格式
+                //    foreach (var c in parameter)
+                //    {
+                //        if (c.Contains("*."))
+                //        {
+                //            format = c.Replace("*", "");
+                //        }
+                //    }
+                //    //整理输入路径
+                //    string[] paths = filename.Split('\\');
+                //    int pathsLength = paths.Length;
+                //    string path = "";
+                //    for (int i = 0; i < pathsLength - 1; i++)
+                //    {
+                //        paths[i] += '\\';
+                //        path += paths[i];
+                //    }
+                //    string lastname = paths[pathsLength - 1];
+
+                //    int j = 0;
+                //    DirectoryInfo dir = new DirectoryInfo(path);
+                //    FileSystemInfo[] inf = dir.GetFileSystemInfos();
+                    
+                //    foreach (FileSystemInfo finf in inf)
+                //    {
+                //        if(finf is DirectoryInfo)//判断是否为文件夹
+                //        {
+                //            DirectoryInfo dinf = new DirectoryInfo(finf.FullName);
+                //            FileInfo[] f = dinf.GetFiles();
+                //            foreach (FileInfo file in f )
+                //            {
+                //                for (i= 0;i<exception)
+                //            }
+                //            //扩展名与条件相符
+                //            if (finf.Extension.Equals(format))
+                    //        //读取文件的完整目录和文件名
+                    //        filename = finf.FullName;
+                    //    MessageBox.Show(filename );
+                    //    if (Directory.Exists(filename))
+                    //    {
+                    //        showdata += "文件" + j + ":\n";
+                    //        Function fc = new Function();
+                    //        fc.CharCount(filename, ref charcount, ref showdata);
+                    //        fc.WordCount(filename, ref wordcount, ref showdata);
+                    //        fc.LineCount(filename, ref linecount, ref showdata);
+                    //        fc.SuperCount(filename, ref notelinecount, ref nulllinecount, ref codelinecount, ref showdata);
+                    //    }
+                    //    j++;
+                    //}
+                //    break;
+               // }
                 //参数-x输出图形界面选择文件并读取文件名
                 if (s == "-x")
                 {
@@ -58,34 +128,25 @@ namespace WC_window_framework
                 //判断文件是否存在
                 if (File.Exists(filename))
                 {
-                    FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
-                    StreamReader sr = new StreamReader(fs);
                     Function fc = new Function();
-                    if (s == "-c"||s=="-x")
+                    if (s == "-c" || s == "-x")
                     {
-                        fc.CharCount(sr, ref charcount);
-                        showdata += "字符数：" + charcount.ToString()+"\n";
+                        fc.CharCount(filename, ref charcount, ref showdata);
                     }
                     if (s == "-w" || s == "-x")
                     {
-                        fc.WordCount(sr, ref workcount);
-                        showdata += "单词数:" + workcount.ToString() + "\n";
+                        fc.WordCount(filename, ref wordcount, ref showdata);
+
                     }
                     if (s == "-l" || s == "-x")
                     {
-                        fc.LineCount(sr, ref linecount);
-                        showdata += "行数：" + linecount.ToString() + "\n";
-                    }
-                    //else if (s == "-")
-                    // {
+                        fc.LineCount(filename, ref linecount, ref showdata);
 
-                    // }
-                    if (s !="-c"&&s!="-w"&&s!="-l"&&s!="-s"&&s!="-a"&&s!="-x")
-                    {
-                        MessageBox.Show("参数输入有误，请重新输入，注意多参数之间必须用空格隔开");
-                        break;
                     }
-                    sr.Close();
+                    if (s == "-a" || s == "-x")
+                    {
+                        fc.SuperCount(filename, ref notelinecount, ref nulllinecount, ref codelinecount, ref showdata);
+                    }
                 }
                 else
                 {
@@ -95,10 +156,7 @@ namespace WC_window_framework
                 //参数为"-x"时只能单独使用，跳出循环
                 if (s == "-x")
                     break;
-
             }
-
-
         }
         /// <summary>
         /// 当参数为"-x"或点击三点按钮时打开图形界面
@@ -108,7 +166,7 @@ namespace WC_window_framework
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.InitialDirectory = "D:\\";
             ofd.Filter = "所有文件|*.*|文本文件|*.txt";
-            ofd.RestoreDirectory = true ;
+            ofd.RestoreDirectory = true;
             ofd.Multiselect = false;//一次只能选择单个文件
             if (ofd.ShowDialog() == DialogResult.OK)
             {
@@ -130,9 +188,14 @@ namespace WC_window_framework
         private void ClearData()
         {
             this.charcount = 0;
-            this.workcount = 0;
+            this.wordcount = 0;
             this.linecount = 0;
             showdata = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- WC window framework/WC window framework/Function.cs
+++ WC window framework/WC window framework/Function.cs
@@ -14,21 +14,27 @@ namespace WC_window_framework
         /// 统计字符数
         /// </summary>
         /// <param name="filename"></param>
-        public void CharCount(StreamReader sr, ref int charcount)
+        public void CharCount(string filename, ref int charcount,ref string showdata)
         {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            StreamReader sr = new StreamReader(fs);
             int srchar;
             while ((srchar = sr.Read()) != -1)
             {
                 charcount++;
             }
+            sr.Close();
+            showdata += "字符数：" + charcount.ToString() + "\n";
         }
         /// <summary>
         /// 统计单词数
         /// </summary>
         /// <param name="sr"></param>
         /// <param name="wordcount"></param>
-        public void WordCount(StreamReader sr, ref int wordcount)
+        public void WordCount(string filename, ref int wordcount,ref string showdata)
         {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            StreamReader sr = new StreamReader(fs);
             char[] symbol = { ' ', ',', '.', '?', '!', ':', ';', '\'', '\"', '\t', '{', '}', '(', ')', '+', '-', '*', '=' };
             int srchar;
             while ((srchar = sr.Read()) != -1)
@@ -41,14 +47,19 @@ namespace WC_window_framework
                     }
                 }
             }
+            wordcount++;
+            sr.Close();
+            showdata += "单词数:" + wordcount.ToString() + "\n";
         }
         /// <summary>
         /// 统计行数
         /// </summary>
         /// <param name="sr"></param>
         /// <param name="linecount"></param>
-        public void LineCount(StreamReader sr, ref int linecount)
+        public void LineCount(string filename, ref int linecount,ref string showdata)
         {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            StreamReader sr = new StreamReader(fs);
             int srchar;
             while((srchar =sr.Read ())!=-1)
             {
@@ -57,6 +68,45 @@ namespace WC_window_framework
                     linecount++;
                 }
             }
+            linecount++;
+            sr.Close();
+            showdata += "行数：" + linecount.ToString() + "\n";
+        }
+        /// <summary>
+        /// 扩展功能：统计代码行、空行、注释行
+        /// </summary>
+        /// <param name="sr"></param>
+        /// <param name="notelinecount"></param>
+        /// <param name="nulllinecount"></param>
+        /// <param name="codelinecount"></param>
+        public void SuperCount (string filename, ref int notelinecount,ref int nulllinecount,ref int codelinecount,ref string showdata)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            StreamReader sr = new StreamReader(fs);
+            string line;
+            while((line =sr.ReadLine ())!=null )
+            {
+                line = line.Trim(' ');
+                line = line.Trim('\t');
+                //空行
+                if(line ==""||line .Length <=1)//代码中单括号为空行
+                {
+                    nulllinecount++;
+                }
+                //注释行
+                else if(line.Substring (0,2)=="//"||line .Substring (1,2)=="//")
+                {
+                    notelinecount++;
+                }
+                //代码行
+                else
+                {
+                    codelinecount++;
+                }
+            }
+            sr.Close();
+            showdata += "代码行：" + codelinecount.ToString() + "\n" + "空行：" + nulllinecount.ToString() + "\n" + "注释行：" + notelinecount.ToString() + "\n";
+
         }
     }
 }
